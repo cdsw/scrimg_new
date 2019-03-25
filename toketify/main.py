51e551e96f0e08c37563fbd23447d9c9a51ba26d
Unknown <punno519@gmail.com>
2019-03-25 22:21:55 +0700

    added toketify program
    Program for squaring images by padding extra dimensions black.

---
+++ toketify/main.py
@@ -0,0 +1,49 @@
+import cv2
+import os
+import shutil
+
+def main():
+    imgCount = 0;
+
+    if not os.path.exists( "./input/" ):
+        os.makedirs( "./input/" )
+
+    if os.path.exists( "./output/" ):
+        shutil.rmtree( "./output/" )
+    else:
+        os.makedirs( "./output/" )
+
+    for root, dirs, files in os.walk( "./input", topdown=False ):
+        for name in files:
+            filepath = os.path.join( root, name )
+
+            fExtArg = filepath.split( "." )[-1]
+
+            if fExtArg not in ["jpg", "png", "jpeg"]:
+                continue
+
+            img = cv2.imread( filepath )
+
+            h, w, ch = img.shape
+            wpad = 0;
+            hpad = 0;
+
+            if h > w:
+                wpad = (h - w)//2
+            if w > h:
+                hpad = (w - h)//2
+
+            procImg = cv2.copyMakeBorder(
+                img,
+                hpad,
+                hpad,
+                wpad,
+                wpad,
+                cv2.BORDER_CONSTANT,
+                value=(0, 0, 0)
+            )
+            outputImg = cv2.imwrite( "./output/X" + str( imgCount ).zfill(5) + ".jpg", procImg )
+            imgCount += 1
+
+if __name__ == "__main__":
+    main()
