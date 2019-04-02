308ca2bef59015f52aff19b3bf384f1f9bfa6551
Unknown <punno519@gmail.com>
2019-04-03 00:25:24 +0700

    Checks for dupes
    + Checks if input image has been processed before
    + Assign input img name to output img name
    - Deleted old test files

--- toketify/main.py
+++ toketify/main.py
@@ -1,30 +1,44 @@
+import re
 import cv2
 import os
-import shutil
 
 def main():
-    imgCount = 0;
+    countoffset = 0
 
     if not os.path.exists( "./input/" ):
         os.makedirs( "./input/" )
-
-    if os.path.exists( "./output/" ):
-        shutil.rmtree( "./output/" )
-    else:
+    if not os.path.exists( "./output/" ):
         os.makedirs( "./output/" )
+
+    if not os.path.exists( "./filelist.txt" ):
+        open( "filelist.txt", "a" ).close()
+
+    if not os.path.exists( "./imgcount.txt" ):
+        newfile = open( "imgcount.txt", "a" )
+        newfile.write("0")
+        newfile.close()
+
+    with open( 'imgcount.txt', 'r' ) as fp:
+        imgcount = int(fp.read())
+    filelist = [line.rstrip( '\n' ) for line in open( 'filelist.txt' )]
 
     for root, dirs, files in os.walk( "./input", topdown=False ):
         for name in files:
+            filename = name.split( "." )[:-1]
+            filename = re.sub( r'[^\w]', '', str( filename ) )
+            if filename in filelist:
+                print( "File " + filename + " skipped (dupe)." )
+                continue
+
             filepath = os.path.join( root, name )
-
             fExtArg = filepath.split( "." )[-1]
-
             if fExtArg not in ["jpg", "png", "jpeg"]:
+                print("File " + filename + " skipped (not image).")
                 continue
 
             img = cv2.imread( filepath )
             h, w, ch = img.shape
-            max_dim = 416
+            max_dim = 352
             #resize image
             if h >= w:
                 new_h, new_w = max_dim, int(max_dim/h * w)
@@ -53,9 +67,22 @@ def main():
                 cv2.BORDER_CONSTANT,
                 value=(0, 0, 0)
             )
-            outputImg = cv2.imwrite( "./output/X" + str( imgCount ).zfill(5) + ".jpg", procImg )
-            
-            imgCount += 1
+            outputImg = cv2.imwrite( "./output/X" + \
+                                     str( imgcount + countoffset ).zfill(5) + "_" + \
+                                     str(filename) + ".jpg", procImg )
+
+            filefl = open("filelist.txt", "a")
+            filefl.write(str(filename)+"\n")
+            filefl.close()
+
+            countoffset += 1
+            print("File " + filename + " processed successfully.")
+
+    fileic = open( "imgcount.txt", "w" )
+    fileic.write( str( imgcount + countoffset ) )
+    fileic.close()
+
+    print("End: " + str(countoffset) + " files processed.")
 
 if __name__ == "__main__":
     main()
